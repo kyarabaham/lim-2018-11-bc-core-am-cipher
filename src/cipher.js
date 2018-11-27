401b586d326ac0f8ee8ca38b8cf79e8ac09eaf46
kbaham <kyarabahamonde09@gmail.com>
2018-11-27 17:33:50 -0500

    Añadiendo test

--- src/cipher.js
+++ src/cipher.js
@@ -1,38 +1,38 @@
 window.cipher = {
- encode: function (offset , string){
- 	 let textCifrado ="";
- 	 for (let i = 0; i < string.length ; i ++){
- 		let codigoAc = string.toUpperCase().charCodeAt(i);
- 		if (codigoAc === 32){
- 			textCifrado += " ";
- 		} else {
- 		let letra = (codigoAc - 65 + parseInt(offset));
- 		let residuo = (letra % 26) + 65;
- 		let resultadoC = String.fromCharCode(residuo);
- 		textCifrado += resultadoC
- 		}
- 	}
- 	return textCifrado;
- }, 
+  encode: function (offset, string) {
+    let textCifrado = "";
+    for (let i = 0; i < string.length; i++) {
+      let codigoAc = string.toUpperCase().charCodeAt(i);
+      if (codigoAc === 32) {
+        textCifrado += " ";
+      } else {
+        let letra = (codigoAc - 65 + parseInt(offset));
+        let residuo = (letra % 26) + 65;
+        let resultadoC = String.fromCharCode(residuo);
+        textCifrado += resultadoC
+      }
+    }
+    return textCifrado;
+  },
 
- decode: function (offset , string){
- 	let textCifrado ="";
- 	for (let i = 0; i < string.length ; i ++){	
- 		let codigoAc = string.toUpperCase().charCodeAt(i);
- 		if (codigoAc === 32){
- 			textCifrado += " ";
- 		} else {
- 			let letra = (codigoAc - 65 - parseInt(offset));
- 			let residuo = (letra % 26) + 65;
- 			if (residuo < 65) {
- 			residuo += 26; 
- 			}
- 			let resultadoC = String.fromCharCode(residuo);
- 			textCifrado += resultadoC;
- 		}
- 	}
- 		return textCifrado;
- }
+  decode: function (offset, string) {
+    let textCifrado = "";
+    for (let i = 0; i < string.length; i++) {
+      let codigoAc = string.toUpperCase().charCodeAt(i);
+      if (codigoAc === 32) {
+        textCifrado += " ";
+      } else {
+        let letra = (codigoAc - 65 - parseInt(offset));
+        let residuo = (letra % 26) + 65;
+        if (residuo < 65) {
+          residuo += 26;
+        }
+        let resultadoC = String.fromCharCode(residuo);
+        textCifrado += resultadoC;
+      }
+    }
+    return textCifrado;
+  }
 
 };
 
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -10,7 +10,12 @@ describe('cipher', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
 
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33');
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33', () =>{
+      assert.equal(cipher.encode(33 ,'ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 'HIJKLMNOPQRSTUVWXYZABCDEFG');
+    });
+    it('deberia retornar "C D" para "A B" con offset 2', () => {
+      assert.equal(cipher.encode(2, 'A B'), 'C D');
+    })
   });
 
   describe('cipher.decode', () => {
@@ -19,10 +24,16 @@ describe('cipher', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33');
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33',()=>{
+      assert.equal(cipher.decode(33, 'HIJKLMNOPQRSTUVWXYZABCDEFG'),'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    });
+    it('deberia retornar "A B" para "C D" con offset 2', () => {
+      assert.equal(cipher.decode(2, 'C D'), 'A B');
+    })
   });
 
-  describe('cipher.createCipherWithOffset', () => {
+
+  /*describe('cipher.createCipherWithOffset', () => {
 
     it('debería ser una función', () => {
       assert.equal(typeof cipher.createCipherWithOffset, 'function');
@@ -30,6 +41,6 @@ describe('cipher', () => {
 
     it('debería retornar un objeto con dos funciones (encode y decode) con offset fijado');
 
-  });
+  });*/
 
 });
