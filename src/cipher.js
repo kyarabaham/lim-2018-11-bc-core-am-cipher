9df4d03a517ea5a43652b2f3fcea4e005f4f05b0
kbaham <kyarabahamonde09@gmail.com>
2018-11-27 12:01:35 -0500

    Separacion de js

--- src/cipher.js
+++ src/cipher.js
@@ -1,47 +1,24 @@
-window.cipher ={
- 
-
-};
-	const efectoCifrar = document.getElementById('btn-txt');
-
- const cipherText = () =>{
- 	let text1 = document.getElementById('text-1').value;
- 	let offset = document.getElementById('despla').value;
- 	console.log(typeof offset)
-  	let textCifrado ="";
-
- 	for (let i = 0; i < text1.length ; i ++){debugger
- 		let codigoAc = text1.toUpperCase().charCodeAt(i);
+window.cipher = {
+ encode: function (offset , string){
+ 	 let textCifrado ="";
+ 	 for (let i = 0; i < string.length ; i ++){
+ 		let codigoAc = string.toUpperCase().charCodeAt(i);
  		if (codigoAc === 32){
  			textCifrado += " ";
-
- 		}else{
+ 		} else {
  		let letra = (codigoAc - 65 + parseInt(offset));
  		let residuo = (letra % 26) + 65;
  		let resultadoC = String.fromCharCode(residuo);
  		textCifrado += resultadoC
-
  		}
- 		
-
- 		document.getElementById('resultado').innerHTML = textCifrado;
  	}
+ 	return textCifrado;
+ }, 
 
- }
- 	efectoCifrar.addEventListener('click', cipherText); 
-
-
-
- 	const efectoDescifrar = document.getElementById('btn-txt2');
- 	
- 	const cipherText1 = () =>{
- 	let text1 = document.getElementById('text-1').value;
- 	let offset = document.getElementById('despla').value;
- 	console.log(typeof offset)
-  	let textCifrado ="";
-   
- 	for (let i = 0; i < text1.length ; i ++){debugger
- 		let codigoAc = text1.toUpperCase().charCodeAt(i);
+ decode: function (offset , string){
+ 	let textCifrado ="";
+ 	for (let i = 0; i < string.length ; i ++){	
+ 		let codigoAc = string.toUpperCase().charCodeAt(i);
  		if (codigoAc === 32){
  			textCifrado += " ";
  		} else {
@@ -53,13 +30,11 @@ window.cipher ={
  			let resultadoC = String.fromCharCode(residuo);
  			textCifrado += resultadoC;
  		}
- 		
-
- 		document.getElementById('resultado').innerHTML = textCifrado;
  	}
-
+ 		return textCifrado;
  }
- 	efectoDescifrar.addEventListener('click', cipherText1); 
+
+};
 
 
 
---
+++ src/index.js
@@ -0,0 +1,23 @@
+const efectoCifrar = document.getElementById('btn-txt');
+
+ const cifrar = () => {
+ 	let string = document.getElementById('text-1').value;
+ 	let offset = document.getElementById('despla').value;
+  	const fCifrar = cipher.encode(offset,string);
+  	document.getElementById('resultado').innerHTML = fCifrar;
+ }
+ 	efectoCifrar.addEventListener('click', cifrar); 
+
+
+
+ 	const efectoDescifrar = document.getElementById('btn-txt2');
+ 	
+ 	const decifrar = () =>{
+ 	let string = document.getElementById('text-1').value;
+ 	
+ 	let offset = document.getElementById('despla').value;
+ 	const dCifrar = cipher.decode(offset,string);
+ 	document.getElementById('resultado').innerHTML = dCifrar;
+  
+ }
+ 	efectoDescifrar.addEventListener('click', decifrar); 
